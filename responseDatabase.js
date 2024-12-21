8f0a3da45a83c1816555be8749fdf4a940f857d8
Quiet0ne <wojciech.kleszcz.2000@gmail.com>
2024-12-21 14:13:39 +0100

    cont1

--- responseDatabase.js
+++ responseDatabase.js
@@ -5,7 +5,16 @@ const responseExamples = [
             sender: "example@company.com",
             subject: "Project Update Meeting",
             content: "Hi, would you be available for a project status update meeting next week?",
-            response: "Hi [Name],\n\nThank you for reaching out. Yes, I would be happy to join the project status update meeting next week.\n\nPlease let me know what times work best for you, and I'll check my calendar.\n\nBest regards,\n[Your name]"
+            response: "Hi [Name],\n\nThank you for reaching out. Yes, I would be happy to join the project status update meeting next week.\n\nPlease let me know what times work best for you, and I'll check my calendar.\n\nBest regards,\n Wojciech Kleszcz"
+        }
+    },
+    {
+        name: "SUCCES WPŁATA",
+        context: {
+            sender: "example@company.com",
+            subject: "SUCCES WPŁATA",
+            content: "Dzień dobry, w załączniku potwierdzenie wpłaty",
+            response: "Dzień dobry, \n\nOdnotowaliśmy Państwa wpłatę. Powiadomienie o opłacie powinno zniknąć w ciągu kilkunastu minut. W razie pytań pozostajemy do dyspozycji :) \n\nMasz pytanie związane z fakturą? Kliknij tutaj i znajdź odpowiedź -> Faktury FAQ \n\nSerdecznie pozdrawiamy i cieszymy się że są Państwo z nami, \n\nWojciech Kleszcz \n\nBilling Specialist \n\ntel.: +48 694 830 608 \nwww.livekid.com"
         }
     }
 ];
